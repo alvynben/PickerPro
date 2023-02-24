d08ca9d2b808f03e4e97248cecc8b877b003ba91
alvynben <alvynben@gmail.com>
2023-02-24 01:04:30 -0800

    feat: add parsing of coordinates from group of invoices

--- batchPick/trialRun.py
+++ batchPick/trialRun.py
@@ -7,6 +7,8 @@ import itertools
 from ast import literal_eval
 import statistics
 import matplotlib.pyplot as plt
+from parse import inventory
+from parse import map as warehouseMap
 
 # Constants
 CSV_FILE_NAME = "trial_data.csv"
@@ -17,11 +19,10 @@ def get_unique_count(df: pd.DataFrame, col_name: pd.Index):
 
 # Extract Data
 df = pd.read_csv(CSV_FILE_NAME)
-print(df.head(10))
 
 # Transform Data
-print(df.iloc[2])
 
+# Invoice Format
 """ Posting No.                                   BCMINV-220500399
 Document No.                                      BCMSO-026974
 Posting Date                                         5/24/2022
@@ -38,44 +39,43 @@ Quantity                                                     3
 Quantity (Base)                                              3 """
 
 invoice_to_weight = pd.DataFrame(df, columns=["Document No.", "Quantity"]).groupby("Document No.")
-# print(invoice_to_weight.loc[invoice_to_weight["DocumentNo."] == "BCMSO-026974"])
-# print(invoice_to_weight.get_group("BCMSO-026974").sum()["Quantity"])
 
 # Apply Knapsack Algorithm on Invoices
-total_no_of_invoices: int = len(df.index)
-invoices_remaining: int = total_no_of_invoices
-MAX_QUANTITY = 40
+total_no_of_orders: int = len(df.index)
+orders_remaining: int = total_no_of_orders
+MAX_QUANTITY = 48
 current_quantity = 0
 current_batch = []
 all_batches = []
 prev_invoice = df.iloc[0]['Document No.']
 
-while invoices_remaining:
-    row_to_check = total_no_of_invoices - invoices_remaining
+while orders_remaining or current_batch:
+    if not orders_remaining:
+        all_batches.append(current_batch)
+        current_batch = []
+        current_quantity = 0
+        break
+    
+    row_to_check = total_no_of_orders - orders_remaining
     current_invoice = df.iloc[row_to_check]['Document No.']
     bound_to_overflow = False
     if current_invoice != prev_invoice:
         bound_to_overflow = current_quantity + invoice_to_weight.get_group(current_invoice).sum()["Quantity"] > MAX_QUANTITY
-        print(current_quantity + invoice_to_weight.get_group(current_invoice).sum()["Quantity"])
-        print("current invoice: ", current_invoice, " prev invoice: ", prev_invoice)
-        print("bound to overflow: ", bound_to_overflow)
+    
+    if not bound_to_overflow:
+        current_batch.append(df.iloc[row_to_check]['No.'])
         prev_invoice = current_invoice
-    
-    
-    if not bound_to_overflow and current_quantity + df.iloc[row_to_check]['Quantity'] <= MAX_QUANTITY:
-        current_batch.append(df.iloc[row_to_check]['Document No.'])
-        invoices_remaining -= 1
+        orders_remaining -= 1
         current_quantity += df.iloc[row_to_check]['Quantity']
     else:
         # add edited batch to all_batches
-        invoices_remaining -=1
         all_batches.append(current_batch)
         current_batch = []
         current_quantity = 0
 
-print(all_batches)
-for batch in all_batches:
-    print(len(batch))
-        
-
 # Apply Pathing on Group of Items
+all_locs = []
+for batch in all_batches:    
+    locs = list(map(lambda x: warehouseMap.get_coordinates(inventory.get_item_location(x)), batch))
+    batch = list(map(lambda x: inventory.get_item_location(x), batch))
+    all_locs.append(locs)
