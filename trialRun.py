5d70cb448f4c7fdefa2ea05c6bfa575e9500a567
kengjit <kengjit123@gmail.com>
2023-03-03 16:43:48 +0800

    Update findPath parameter type Add pathfinding algo to trial run

--- trialRun.py
+++ trialRun.py
@@ -9,6 +9,7 @@ import statistics
 import matplotlib.pyplot as plt
 from batchPick.utils.parse import inventory
 from batchPick.utils.parse import map as warehouseMap
+from findPath import findPath
 
 # Constants
 CSV_FILE_NAME = "batchPick/trainingData/trial_data.csv"
@@ -22,7 +23,7 @@ df = pd.read_csv(CSV_FILE_NAME)
 
 # Transform Data
 invoice_to_weight = pd.DataFrame(df, columns=["Document No.", "Quantity"]).groupby("Document No.")
-
+print(invoice_to_weight)
 # Apply Knapsack Algorithm on Invoices
 total_no_of_orders: int = len(df.index)
 orders_remaining: int = total_no_of_orders
@@ -56,9 +57,39 @@ while orders_remaining or current_batch:
         current_batch = []
         current_quantity = 0
 
-# Apply Pathing on Group of Items
+# Apply Pathing on Group of Orders
 all_locs = []
 for batch in all_batches:    
     locs = list(map(lambda x: warehouseMap.get_coordinates(inventory.get_item_location(x)), batch))
     batch = list(map(lambda x: inventory.get_item_location(x), batch))
     all_locs.append(locs)
+
+all_locs = [
+    [(8, 71), (12, 59), (5, 25), (6, 8), (14, 12), (16, 25), (17, 61)],
+    [(15, 13), (17, 29), (7, 13)],
+    [(16, 61), (12, 59), (13, 43), (5, 0), (6, 0), (14, 59), (2, 17), (14, 59), (14, 12), (11, 60)], 
+    [(8, 76)]
+    ]
+all_batches = [
+    ['ZZASAF50GM', 'ZZBGF1KG', 'ZZRF600GM', 'SAPUFCM1LTR', 'ZZBFCLD3LTR', 'ZZFSH1KG', 'ZZSTP500GM'], 
+    ['ZZKNCSBL480GM', 'ZZKAPHPS6KG', 'ZZMGCCS1200GM'], 
+    ['ZZCHP250GM', 'ZZUDS1KG', 'SPTCIO1880GM', 'ZZNFBL3400ML', 'ZZNFDWL5LTR', 'DZRCD500GM', 'MGMP3KG', 'DZCORP500GM', 'ZZBFCLD3LTR', 'YWBRPUP1KG'], 
+    ['ZZBSH2250GM']]
+
+# Get all order ids in order
+orders = []
+for i in range(len(all_locs)):
+    order_coords_idx = findPath.findPath(all_locs[i], warehouseMap.map)
+    order_ids = []
+    for coord_idx in order_coords_idx:
+        order_ids.append(all_batches[i][coord_idx])
+    orders.append(order_ids)
+
+# Get each order's item details
+for order in orders:
+    order_data = []
+    for item in order:
+        sku = inventory.find_item(item)
+        order_data.append([sku.bin_location, sku.id_no, sku.desc, sku.packing_size, sku.unit_of_measurement])
+    df = pd.DataFrame(order_data, columns=["Bin", "No.", "Description", "Packing Size", "UOM"])
+    print(df, "\n")
